8688dca9acaf1af5f8e3d3f9a460f4b000245b9c
yanntrc <yann.trc@tutanota.com>
2025-07-02 21:06:14 +0200

    fix config tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-
-import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Blog from './pages/Blog'
+import './App.css'
 
 function App() {
   
 
   return (
+    
   <BrowserRouter>
       <nav style={{
         display: 'flex',
@@ -20,6 +20,13 @@ function App() {
         <Link to="/projects">Projects</Link>
         <Link to="/blog">Blog</Link>
       </nav>
+      <div className="space-y-4 p-8">
+  <p className="text-red-500 font-bold text-lg">Ceci est un texte rouge</p>
+  <p className="text-green-500 font-semibold">Ceci est un texte vert</p>
+  <p className="text-blue-500 text-xl">Ceci est un texte bleu</p>
+  <p className="text-purple-600 italic">Ceci est un texte violet</p>
+  <p className="text-yellow-400 underline">Ceci est un texte jaune</p>
+</div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,19 @@
 function Home() {
-  return <div>Home page!</div>
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-100">
+      <section className="bg-blue-500 shadow-2xl rounded-2xl px-10 py-12 max-w-md w-full text-center">
+        <h1 className="text-4xl font-extrabold text-white mb-3 tracking-tight">
+          Yann Troadec
+        </h1>
+        <h2 className="text-xl font-semibold text-blue-100 mb-6">
+          Web & Blockchain Developer
+        </h2>
+        <div className="bg-green-600 text-white p-8">TEST VERT</div>
+        <div className="text-blue-100 font-medium mb-1">France</div>
+      </section>
+      
+    </main>
+  )
 }
+
 export default Home
